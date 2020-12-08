941cc5c39de286b589f070f44e14f373ea24e6c8
Tolu Opadele <toluuro@gmail.com>
2020-12-08 20:51:06 +0100

    Updates

---
+++ precache-manifest.1f5bb6fce4b4ce9d9d03683f1c245600.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe1e3fafe233006f7cd596594431919",
+    "url": "/Redux-RobotFriends-WebApp/index.html"
+  },
+  {
+    "revision": "117209961bb2d1740fe1",
+    "url": "/Redux-RobotFriends-WebApp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e8f4764258f2cc5cbc3a",
+    "url": "/Redux-RobotFriends-WebApp/static/css/main.dbd75dd6.chunk.css"
+  },
+  {
+    "revision": "117209961bb2d1740fe1",
+    "url": "/Redux-RobotFriends-WebApp/static/js/2.d14af121.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Redux-RobotFriends-WebApp/static/js/2.d14af121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f4764258f2cc5cbc3a",
+    "url": "/Redux-RobotFriends-WebApp/static/js/main.60370934.chunk.js"
+  },
+  {
+    "revision": "7fe0a9fc3da289574810",
+    "url": "/Redux-RobotFriends-WebApp/static/js/runtime-main.c618f61f.js"
+  },
+  {
+    "revision": "92c3810f48104a0fa26cbef3ad302807",
+    "url": "/Redux-RobotFriends-WebApp/static/media/electrical.92c3810f.woff"
+  }
+]);
